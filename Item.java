efe57e0089be246e527e29a48f7eac190b601c27
Max Graff <maksymilian.graff@gmail.com>
2024-10-19 21:44:48 +0200

    Refactoring: removed Entity, replaced with Item; Removed Block, probably will redeclare

---
+++ Item.java
@@ -0,0 +1,23 @@
+
+
+
+
+public class Item {
+
+
+    private String name;
+    private ItemType type;
+    private String imagePath;
+    private int maxNum;
+
+    Item(String name, ItemType type, String imagePath, int maxNum)
+    {
+        this.name = name;
+        this.type = type;
+        this.imagePath = imagePath;
+        this.maxNum = maxNum;
+    }
+
+
+    
+}
